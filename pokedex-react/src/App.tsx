547bb05d6ec78e256803782e20732a916c10c905
IkenoXamos <moberlies1337@gmail.com>
2021-10-12 14:55:24 -0400

    Add React bootstrap with basic forms

--- pokedex-react/src/App.tsx
+++ pokedex-react/src/App.tsx
@@ -1,33 +1,63 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { getPokemon } from './remote/poke-api/poke.api';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button } from 'react-bootstrap';
 
 const App: React.FC<unknown> = () => {
 
+  const [name, setName] = useState("");
+  const [info, setInfo] = useState({
+    id: 0,
+    name: "",
+    weight: 0,
+    height: 0,
+  });
+
   useEffect( () => {
     (async () => {
-      console.log(await getPokemon('arcanine'));
-    });
+      try {
+        console.log(await getPokemon(59));
+      } catch(error) {
+        console.log(error);
+      }
+    })();
   }, []);
 
+  const searchPokemon = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      let p = await getPokemon(name);
+      console.log(p);
+      setInfo({
+        id: p.id,
+        name: p.name,
+        weight: p.weight,
+        height: p.height
+      });
+    } catch (error) {
+      alert(`Pokemon ${name} not found!`);
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form onSubmit={(e) => searchPokemon(e)}>
+        <Form.Group className="mb-3">
+          <Form.Label>Pokemon Name</Form.Label>
+          <Form.Control required type="text" placeholder="blue eyes white dragon" onChange={(e) => setName(e.target.value)}/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Search for Pokemon
+        </Button>
+      </Form>
+      <br />
+      <p>
+        id: {info.id} <br/>
+        name: {info.name} <br/>
+        height: {info.height} <br/>
+        weight: {info.weight} <br/>
+      </p>
     </div>
   );
 }
--- pokedex-react/src/remote/poke-api/poke.api.ts
+++ pokedex-react/src/remote/poke-api/poke.api.ts
@@ -2,11 +2,7 @@ import Pokemon from '../../models/pokemon';
 import pokeClient from './poke.client';
 
 export const getPokemon = async (id: string | number): Promise<Pokemon> => {
-    const { data: pokemon } = await pokeClient.get<Pokemon>(`/api/Pokemon/${id}`);
-
-    console.log(pokemon);
-    console.log('id' in pokemon); // true/false
-    // Which is only here as a sanity check in case something goes wrong
+    const { data: pokemon } = await pokeClient.get<Pokemon>(`/pokemon/${id}`);
 
     return pokemon;
 }
