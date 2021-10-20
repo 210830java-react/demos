2b838ecdcc8c892b8c85e44095fa9231a6e5b829
wsingleton <wezley.singleton@gmail.com>
2021-10-20 14:53:59 -0400

    swapped post mapping method responseentity for annotations + exception handler

--- microservices/quiz-service/src/main/java/com/revature/controllers/QuizController.java
+++ microservices/quiz-service/src/main/java/com/revature/controllers/QuizController.java
@@ -3,14 +3,11 @@ package com.revature.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import com.revature.exceptions.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import com.revature.models.Flashcard;
@@ -59,15 +56,16 @@ public class QuizController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Quiz> insert(@RequestBody Quiz quiz) {
+	@ResponseStatus(HttpStatus.CREATED)
+	public Quiz insert(@RequestBody Quiz quiz) {
 		int id = quiz.getId();
 		
 		if(id != 0) {
-			return ResponseEntity.badRequest().build();
+			throw new BadRequestException("Invalid id provided for new resource.");
 		}
 		
 		quizDao.save(quiz);
-		return ResponseEntity.status(201).body(quiz);
+		return quiz;
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -81,4 +79,9 @@ public class QuizController {
 		
 		return ResponseEntity.ok(all);
 	}
+
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+	public void handleBadRequest(BadRequestException e) {}
+
 }
