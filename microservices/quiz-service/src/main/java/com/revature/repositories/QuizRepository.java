86c65b415fb85427c0a43228102a4bde536b5a1a
wsingleton <wezley.singleton@gmail.com>
2021-10-20 14:55:17 -0400

    added note on optional @Repository anno

--- microservices/quiz-service/src/main/java/com/revature/repositories/QuizRepository.java
+++ microservices/quiz-service/src/main/java/com/revature/repositories/QuizRepository.java
@@ -3,7 +3,9 @@ package com.revature.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.models.Quiz;
+import org.springframework.stereotype.Repository;
 
+//@Repository is optional since Spring Data will automatically detect extensions of JpaRepository
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
 
 }
